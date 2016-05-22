7f25f3a6bcc90bb04b6d8ed9098917bb1bef4ccd
Paul Kinlan <paul.kinlan@gmail.com>
2016-05-22 15:10:23 -0700

    Missing arrow

--- app/sw.js
+++ app/sw.js
@@ -42,7 +42,7 @@ self.addEventListener('activate',  event => {
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
+    caches.match(event.request).then(response => {
       return response || fetch(event.request);
     })
   );
